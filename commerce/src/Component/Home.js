c2c3d5aaca3d84c9de3ece6fd9f5fffd48a8a34c
amaar raza <razaammar582@gmail.com>
2022-11-01 09:22:50 +0500

    E commerceweb page updated

---
+++ commerce/src/Component/Home.js
@@ -0,0 +1,12 @@
+import React, { useRef, useState } from "react";
+import Navbar from './Navbar'
+const Home = () => {
+  return (
+         <>
+         <Navbar/>
+      
+         </>
+                    )
+}
+
+export default Home
